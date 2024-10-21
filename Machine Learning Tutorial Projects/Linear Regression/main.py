b4212ce18caa11a486aaf3e69ff60312e9b64b72
liamc2042 <liam_clark2042@hotmail.com>
2024-10-21 15:43:36 -0400

    ML Porjects
    Added ML projects from desktop to repo

---
+++ Machine Learning Tutorial Projects/Linear Regression/main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+X = np.array(data.drop([predict], axis=1))
+Y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+"""
+best = 0
+for _ in range(30):
+
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+
+    acc = linear.score(x_test, y_test)
+
+    print(acc)
+
+    if acc > best:
+        with open("student_model.pickle", "wb") as f:
+            pickle.dump(linear, f)
+            best = acc"""
+
+pickle_in = open("student_model.pickle", "rb")
+
+linear = pickle.load(pickle_in)
+
+print("CO: \n",  linear.coef_)
+print("Intercept: \n",  linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
+p = "G1"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
